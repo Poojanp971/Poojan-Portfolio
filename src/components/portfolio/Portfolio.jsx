a2d9896f06809f319735fc66c5274094b37baf8d
Poojan Patel <poojanp971@gmail.com>
2024-01-11 19:37:46 -0500

    Portfolio Initial Commit

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,85 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "Euphoria E-Commerce",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "An E-commerce platform build with MERN which was the project that I made in College, sellers can sell products, Buyers can buy and you can keep track of the profuct you added and what you want to buy. It has an admin panel where admin can keep track of users and thier products.",
+  },
+  {
+    id: 2,
+    title: "Multiplayer Snake Game",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: 'It is a classic snake game but it can be played by two people just by sharing you game code. Who ever dies early or whoever has highest point at the end Wins. It is made with Socket.io',
+  },
+  {
+    id: 3,
+    title: "Netffex - A netflix Clone",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Neffex is a clone app of Netflix app, where you can see popular show's trailers on the same screen just by clicking on it. It is made with React and TMDB API",
+  },
+  {
+    id: 4,
+    title: "Music App",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section >
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{y}}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,117 @@
+import { useRef } from "react";
+import "./services.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      animate="animate"
+      whileInView="animate"
+      ref={ref}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on helping your brand
+          <br /> to grow online
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{color:"orange"}}>For Your</motion.b> Digital Footprint.
+          </h1>
+          <button>WHAT I DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Design</h2>
+          <p>
+            A skilled developer proficient in crafting visually appealing and intuitive designs,
+            this professional excels in creating meticulous wireframes with a keen eye for
+            aesthetics and seamless user flow. With a focus on both form and function, they
+            bring a harmonious blend of design and functionality to their projects.
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Frontend UI</h2>
+          <p>
+            Specializing in implementing pixel-perfect UI designs. This expert seamlessly
+            translates intricate design concepts into flawless, pixel-precise interfaces. Their
+            dedication to detail ensures a visually stunning and polished user experience,
+            demonstrating a commitment to achieving perfection in every aspect of UI implementation.
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Backend Solution</h2>
+          <p>
+            An adept backend developer renowned for creating robust and scalable server-side
+            solutions. This professional excels in architecting and implementing backend systems
+            with a focus on reliability, performance, and security. Their expertise ensures seamless
+            integration, efficient data management, and a strong foundation for the overall stability
+            of applications.
+          </p>
+          <button>Go</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Functionality</h2>
+          <p>
+            Proficient in translating project requirements into tangible product features. This
+            expert excels in the meticulous implementation of features, ensuring they align seamlessly
+            with project objectives. With a keen understanding of user needs, this professional
+            contributes to the successful realization of project goals, delivering impactful and
+            well-executed features.
+          </p>
+          <button>Go</button>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
